faec1ac5ea6ae0d0338077d717cd1e477c90ce8a
David Sansome <me@davidsansome.com>
2010-05-24 10:51:41 +0000

    Generate the templates with a custom command so we don't recompile everything each time
    git-svn-id: https://gstreamer-cmake.googlecode.com/svn/trunk@12 7c445bf5-fefb-3aac-dfa0-090326a05c82

--- CMake/GLibHelpers.cmake
+++ CMake/GLibHelpers.cmake
@@ -54,66 +54,17 @@ macro(add_glib_enumtypes outfiles name includeguard)
   set(ctemplate "${CMAKE_CURRENT_BINARY_DIR}/${name}.c.template")
 
   # Write the .h template
-  file(WRITE ${htemplate} "/*** BEGIN file-header ***/
-#ifndef __${includeguard}_ENUM_TYPES_H__
-#define __${includeguard}_ENUM_TYPES_H__
-
-#include <glib-object.h>
-
-G_BEGIN_DECLS
-
-/*** END file-header ***/
-/*** BEGIN file-production ***/
-
-/* enumerations from \"@filename@\" */
-
-/*** END file-production ***/
-/*** BEGIN value-header ***/
-GType @enum_name@_get_type (void);
-#define GST_TYPE_@ENUMSHORT@ (@enum_name@_get_type())
-
-/*** END value-header ***/
-/*** BEGIN file-tail ***/
-G_END_DECLS
-
-#endif /* __${includeguard}_ENUM_TYPES_H__ */
-/*** END file-tail ***/
-")
-
-  # Write the .c template
-  file(WRITE ${ctemplate} "/*** BEGIN file-header ***/
-#include \"${name}.h\"\n")
-  foreach(header ${ARGN})
-    file(APPEND ${ctemplate} "#include \"${header}\"\n")
-  endforeach(header)
-  file(APPEND ${ctemplate} "
-/*** END file-header ***/
-/*** BEGIN file-production ***/
-
-/* enumerations from \"@filename@\" */
-/*** END file-production ***/
-/*** BEGIN value-header ***/
-GType
-@enum_name@_get_type (void)
-{
-  static volatile gsize g_define_type_id__volatile = 0;
-  if (g_once_init_enter (&g_define_type_id__volatile)) {
-    static const G@Type@Value values[] = {
-/*** END value-header ***/
-/*** BEGIN value-production ***/
-{ @VALUENAME@, \"@VALUENAME@\", \"@valuenick@\" },
-/*** END value-production ***/
-/*** BEGIN value-tail ***/
-{ 0, NULL, NULL }
-    };
-    GType g_define_type_id = g_@type@_register_static (\"@EnumName@\", values);
-    g_once_init_leave (&g_define_type_id__volatile, g_define_type_id);
-  }
-  return g_define_type_id__volatile;
-}
-
-/*** END value-tail ***/
-")
+  add_custom_command(
+    OUTPUT ${htemplate} ${ctemplate}
+    COMMAND ${CMAKE_COMMAND}
+        "-Dctemplate=${ctemplate}"
+        "-Dhtemplate=${htemplate}"
+        "-Dname=${name}"
+        "-Dincludeguard=${includeguard}"
+        "\"-Dheaders=${ARGN}\""
+        -P "${CMAKE_SOURCE_DIR}/CMake/MakeGLibEnumTemplates.cmake"
+    DEPENDS "${CMAKE_SOURCE_DIR}/CMake/MakeGLibEnumTemplates.cmake" ${headers}
+  )
 
   add_glib_enumtypes_t(${outfiles} ${name} ${htemplate} ${ctemplate} ${ARGN})
 endmacro(add_glib_enumtypes)
